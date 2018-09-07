c68809d11378b1654f5a4de3d2d6e5ce23b1e8ed
Stephen Fleming <fleming.steve72@gmail.com>
2018-09-07 12:33:35 +1000

    page list view now shows correct titles

--- src/templates/menuPage.js
+++ src/templates/menuPage.js
@@ -10,23 +10,38 @@ class MenuPage extends Component {
       menuName: '',
       menuSlug: '',
       pageList: [],
+      language: 'en'
     };
 
     this.mapPageListDataToElem = this.mapPageListDataToElem.bind(this);
+    this.getCardTitleForLanguage = this.getCardTitleForLanguage.bind(this);
   }
 
   componentWillMount() {
     this.setState({
-      menuName: this.props.data.contentfulMenuItem.name,
+      menuName: this.props.data.contentfulMenuItem.title,
       menuSlug: this.props.data.contentfulMenuItem.slug,
       pageList: this.props.data.contentfulMenuItem.pageList || [],
     });
   }
 
+  getCardTitleForLanguage(card, language) {
+    card.forEach( card => {
+      if (card.language.code === language)
+        return card.titleText;
+    });
+
+    //else revert to default
+    return card[0].titleText;
+    
+  }
+
   mapPageListDataToElem() {
     return this.state.pageList.map((item, index) => {
       const url = `${this.state.menuSlug}/${item.slug}`;
-      const title = item.title;
+
+      //todo: user language choice
+      const title = this.getCardTitleForLanguage(item.titleList, "en");
 
       return (
         <div className="content" key={index}>
@@ -65,6 +80,12 @@ export const pageQuery = graphql`
       }
       pageList {
         slug
+        titleList {
+          titleText
+          language {
+            code
+          }
+        }
       }
     }
   }
