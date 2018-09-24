2c302fde149a047a423b0f730d0c41fbbccfb0bb
Stephen Fleming <fleming.steve72@gmail.com>
2018-09-24 10:55:31 +1000

    added clinic page under pages

---
+++ src/pages/clinic-locations.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MapContainer from '../components/Map/MapContainer.js';
+
+const ClinicLocations = props => {
+	return(<MapContainer/>);
+};
+
+export default ClinicLocations;
