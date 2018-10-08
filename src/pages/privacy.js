0b2c568da8c639ef2af8d72f52b4b9553c30af21
Zach Keuken <zachkeuken@gmail.com>
2018-10-08 12:08:55 +1100

    Add Privacy Policy and Terms of Use Pages

---
+++ src/pages/privacy.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const PrivacyPage = props => {
+  return (
+    <div className="section">
+      <div className="content">
+        <h1>Privacy Policy</h1>
+        <p>
+		  This Privacy Policy pertains to the website(s) hosted by Shifra (<strong>"Shifra"</strong>), ABN: 90 794 036 744. 
+		  This includes www.shifra.io and additional produced and managed websites by Shifra (together, the 
+		  <strong> "Site"</strong>). Shifra reserves the right, at its sole discretion, to alter and update this 
+		  Privacy Policy from time to time; therefore, we urge visitors to our Site to review the current version of the 
+		  Privacy Policy each time they return to our Site. The date of the more recent update is reflected at the bottom 
+		  of this document.
+		</p>
+		<p>
+		  When you request information from Shifra and/or supply information through the Site that personally identifies 
+		  you and/or allows us to contact you including any and all materials submitted by you in connection with registering 
+		  for email subscriptions, including but not limited to, when you fill out a contact form or expertise inquiry, 
+		  opt in to receive emails from Shifra, or agree to participate in surveys, you are agreeing to share such 
+		  information, including your name, e-mail address, region to Shifra, reason for contacting Shifra, and any message 
+		  you submit, with Shifra, its agents, representatives and affiliates, and you should know that Shifra may disclose 
+		  such information to its representatives and affiliates for marketing and promotional purposes. Shifra may aggregate 
+		  personally identifiable information and may disclose such information in aggregate for marketing and promotional 
+		  purposes. However, in these situations, we do not disclose to these entities any information that could be used to 
+		  personally identify you. We may collect the e-mail addresses of those who send us e-mail messages; however, we will 
+		  not send unsolicited e-mail to any of the addresses we collect or share those addresses with any unaffiliated third 
+		  party, except in the limited circumstances set forth below. Shifra would like to keep your personal information 
+		  accurate.
+		</p>
+		<p>
+		  Shifra has implemented generally accepted standards of technology and operational security in order to protect your 
+		  personally identifiable information from loss, misuse, alteration, or destruction. Despite these precautions, 
+		  however, Shifrac annot guarantee that unauthorized persons will not obtain access to your personally identifiable 
+		  information.
+		</p>
+		<p>
+		  Shifra may disclose your Personal Data in order to enforce our website Terms of Use or in urgent circumstances to 
+		  protect personal safety, the public or Shifra’s websites. It is also possible that we may be required by law to 
+		  disclose your Personal Data pursuant to judicial or other government subpoenas, warrants, or orders, potentially, 
+		  in any country where we do business.
+		</p>
+		<p>
+		  This Privacy Policy does not apply to any information you may disclose publicly in any chat rooms, message boards, 
+		  or similar web pages, including those hosted on or linked to our Site. You should keep in mind that whenever you 
+		  publicly disclose information about yourself online—for example, via message boards or chat rooms—that information 
+		  could be collected and used by people whom you do not know. In addition, certain message boards and similar user 
+		  forums may display IP addresses along with the message poster's name and message. Shifra bears no responsibility 
+		  for any action or policies of any third parties who collect any information users may disclose on the message 
+		  boards, chat areas or other user forums, if any, on the Site.
+		</p>
+		<p>
+		  This Policy was last updated on 15 March 2018. In order to check updates to this Policy, please consult this page 
+		  on a regular basis.
+		</p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPage;
